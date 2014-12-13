c46639fbc695a77c70a125ad4ab4fe7a1b4742da
Paul Bourke <pauldbourke@gmail.com>
2014-12-13 13:47:32 +0000

    Add stop functionality to reset clock when time up
    Also indent fixups (use git diff -w)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,76 +8,83 @@
  * Controller of the brewtimerApp
  */
 angular.module('brewtimerApp')
-  .controller('MainCtrl', function ($scope, ngAudio) {
+    .controller('MainCtrl', function ($scope, ngAudio) {
 
-    $scope.alarm = ngAudio.load('sounds/alarm.mp3');
-    $scope.alarm.loop = true;
-    $scope.showAlert = false;
+  $scope.alarm = ngAudio.load('sounds/alarm.mp3');
+  $scope.alarm.loop = true;
 
-    $scope.brewAdditions = [
-      {
-          'ingredient': '',
-          'mins': 60,
-          'amount': 14,
-          'unit': 'g'
-      },
-      {
-          'ingredient': '',
-          'mins': 10,
-          'amount': 28,
-          'unit': 'g'
-      },
-      {
-        'ingredient': '',
-        'mins': 0,
-        'amount': 28,
-        'unit': 'g'
-      }
-    ];
+  $scope.showAlert = false;
 
-    $scope.timerRunning = false;
+  $scope.timerButtonText = 'Start';
 
-    $scope.onTimerButtonClick = function () {
-      if ($scope.timerRunning) {
-        $scope.$broadcast('timer-stop');
-        $scope.timerRunning = false;
-      } else {
-        $scope.$broadcast('timer-start');
-        $scope.timerRunning = true;
-      }
-    };
+  $scope.brewAdditions = [
+    {
+      'ingredient': '',
+      'mins': 60,
+      'amount': 14,
+      'unit': 'g'
+    },
+    {
+      'ingredient': '',
+      'mins': 10,
+      'amount': 28,
+      'unit': 'g'
+    },
+    {
+      'ingredient': '',
+      'mins': 0,
+      'amount': 28,
+      'unit': 'g'
+    }
+  ];
 
-    //$scope.onResetButtonClick = function() {
-        //$scope.$broadcast('timer-clear');
-        //$scope.timerRunning = false;
-    //};
-
-    $scope.$on('timer-tick', function (event, args) {
-      var curTimeSeconds = args.millis / 1000;
-      $scope.brewAdditions.forEach(function(item) {
-        if (item.mins * 60 === curTimeSeconds) {
-          $scope.alarm.play();
-          $scope.nextAddition = item;
-          $scope.showAlert = true;
-        }
-      });
-    });
-
-    $scope.addBrewAddition = function() {
-      $scope.brewAdditions.push({
-          'ingredient': '',
-          'mins': 0,
-          'amount': 28.34,
-          'unit': 'g'
-      });
-    };
-
-    $scope.removeBrewAddition = function() {
-      $scope.brewAdditions.pop();
-    };
-
-    $scope.onAlertClick = function() {
+  $scope.onTimerButtonClick = function () {
+    if ($scope.timerButtonText === 'Pause') {
+      $scope.$broadcast('timer-stop');
+      $scope.timerButtonText = 'Start';
+    } else if ($scope.timerButtonText === 'Start') {
+      $scope.$broadcast('timer-start');
+      $scope.timerButtonText = 'Pause';
+    } else if ($scope.timerButtonText === 'Stop') {
       $scope.alarm.stop();
       $scope.showAlert = false;
-    };
+      $scope.timerButtonText = 'Start';
+      // NOTE(bourke): hacky way to reset timer
+      $scope.$broadcast('timer-stop');
+      $scope.$broadcast('timer-start');
+      $scope.$broadcast('timer-stop');
+    }
+  };
+
+  $scope.$on('timer-tick', function (event, args) {
+    var curTimeSeconds = args.millis / 1000;
+    $scope.brewAdditions.forEach(function(item) {
+      if (item.mins * 60 === curTimeSeconds) {
+        $scope.alarm.play();
+        $scope.nextAddition = item;
+        $scope.showAlert = true;
+      }
+      if (curTimeSeconds === 0) {
+        $scope.timerButtonText = 'Stop';
+      }
+    });
   });
+
+  $scope.onAlertClick = function() {
+    $scope.alarm.stop();
+    $scope.showAlert = false;
+  };
+
+  $scope.addBrewAddition = function() {
+    $scope.brewAdditions.push({
+        'ingredient': '',
+        'mins': 0,
+        'amount': 28.34,
+        'unit': 'g'
+    });
+  };
+
+  $scope.removeBrewAddition = function() {
+    $scope.brewAdditions.pop();
+  };
+});
